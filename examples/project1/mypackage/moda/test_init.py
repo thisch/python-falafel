dc6611b444da76d35e67a7f300cf69c651aea82c
Thomas Hisch <t.hisch@gmail.com>
2013-11-20 18:41:12 +0100

    add log msg to DummyTest

--- examples/project1/mypackage/moda/test_init.py
+++ examples/project1/mypackage/moda/test_init.py
@@ -5,11 +5,13 @@ class DummyTest(TestBase):
 
     def _do_test_abc(self, x):
         self.created_files.append(x)
+        self.log.debug("added file %s", x)
+        self.log.error("bla")
         self.assertTrue(x)
 
     @classmethod
     def createTests(cls):
         funcs = {}
-        funcs['test_mysuper'] = lambda s, v="blub": s._do_test_abc(v)
-        funcs['test_myfoo'] = lambda s, v=False: s._do_test_abc(v)
+        funcs['test_mysuper'] = lambda s, v="test.rst": s._do_test_abc(v)
+        funcs['test_myfoo'] = lambda s, v="foobar.csv": s._do_test_abc(v)
         cls._createTests(globals(), **funcs)
