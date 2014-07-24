1968e5ebea6b6460f978afea52c38d22b59afeee
Thomas Hisch <t.hisch@gmail.com>
2014-07-24 17:29:48 +0200

    [examples] minor stylistic cleanup

--- examples/project1/mypackage/modb/test_bar.py
+++ examples/project1/mypackage/modb/test_bar.py
@@ -4,7 +4,7 @@ from .. import TestBase
 class TestBar(TestBase):
     def test_print(self):
 
-        self.log.info("one of the tests in test_bar.py\n" + '\n'.join(
-            [str(x) for x in range(10)]))
+        self.log.info("one of the tests in test_bar.py\n" +
+                      '\n'.join([str(x) for x in range(10)]))
         self.assertTrue(3+4 > 2)
         self.log.debug("looks good")
